7b93722767f94634491f4922f403dac33574cb85
Mariana Cedica <mcedica@nuxeo.com>
2019-02-25 13:58:02 -0800

    FV-4: refactor workers

--- FirstVoicesExport/src/main/java/ca/firstvoices/workers/FVExportWorker.java
+++ FirstVoicesExport/src/main/java/ca/firstvoices/workers/FVExportWorker.java
@@ -1,19 +1,20 @@
 package ca.firstvoices.workers;
 
-import ca.firstvoices.format_producers.FV_AbstractProducer;
-import ca.firstvoices.format_producers.FV_PhraseCSVProducer;
-import ca.firstvoices.property_readers.FV_DataBinding;
-import ca.firstvoices.format_producers.FV_WordCSVProducer;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.nuxeo.ecm.core.api.*;
-import org.nuxeo.runtime.api.Framework;
-
-import javax.security.auth.login.LoginContext;
-import java.util.List;
-
 import static ca.firstvoices.utils.FVExportConstants.FVWORD;
 import static ca.firstvoices.utils.FVExportConstants.ON_DEMAND_WORKER_CATEGORY;
+
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.nuxeo.ecm.core.api.DocumentLocation;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.IdRef;
+
+import ca.firstvoices.format_producers.FV_AbstractProducer;
+import ca.firstvoices.format_producers.FV_PhraseCSVProducer;
+import ca.firstvoices.format_producers.FV_WordCSVProducer;
+import ca.firstvoices.property_readers.FV_DataBinding;
 
 /*
         Worker description is in FVAbstractExportWorker file.
@@ -40,67 +41,61 @@ public class FVExportWorker extends FVAbstractExportWork {
 
     @Override
     public void work() {
-        try {
-            if (!getDocuments().isEmpty()) {
-                LoginContext lctx = Framework.login();
-                CoreSession session = CoreInstance.openCoreSession("default");
+        if (!getDocuments().isEmpty()) {
+            openSystemSession();
 
-                List listToProcess = getDocuments();
+            List listToProcess = getDocuments();
 
-                FV_AbstractProducer fileOutputProducer;
+            FV_AbstractProducer fileOutputProducer;
 
-                if (workInfo.exportElement.equals(FVWORD)) {
-                    fileOutputProducer = new FV_WordCSVProducer(session, workInfo.fileName, workInfo.columns);
-                } else {
-                    fileOutputProducer = new FV_PhraseCSVProducer(session, workInfo.fileName, workInfo.columns);
-                }
-
-                fileOutputProducer.writeColumnNames();
-
-                int size = listToProcess.size();
-                double originalSize = size;
-                int counter = 1;
-
-                workInfo.setExportProgress("Exporting... " + size + " words.");
-
-                while (!listToProcess.isEmpty()) {
-                    size = listToProcess.size();
-
-                    if (counter % 51 == 0) {
-                        counter = 0;
-                        double currentSize = size;
-                        double percent = round(100 * (1 - (currentSize / originalSize)), 1);
-                        workInfo.setExportProgressValue(percent);
-                        workInfo.setExportProgress(percent + "% done.");
-                    }
-
-                    DocumentLocation docLocation = (DocumentLocation) listToProcess.get(size - 1);
-                    listToProcess.remove(size - 1);
-
-                    if (docLocation != null) {
-                        DocumentModel doc = session.getDocument(docLocation.getIdRef());
-
-                        if (doc != null) {
-                            List<FV_DataBinding> output = fileOutputProducer.readPropertiesWithReadersFrom(doc);
-
-                            assert (output != null) : "Null output from producer";
-
-                            if (output != null) {
-                                fileOutputProducer.writeRowData(output);
-                                counter++;
-                            }
-                        }
-                    } else {
-                        log.warn("NUll docLocation in FVExportWorker.");
-                    }
-                }
-
-                fileOutputProducer.close(session.getDocument(new IdRef(getDialectGUID())), getWorkInfo());
-                lctx.logout();
+            if (workInfo.exportElement.equals(FVWORD)) {
+                fileOutputProducer = new FV_WordCSVProducer(session, workInfo.fileName, workInfo.columns);
+            } else {
+                fileOutputProducer = new FV_PhraseCSVProducer(session, workInfo.fileName, workInfo.columns);
             }
-        } catch (Exception e) {
-            log.warn(e);
-            e.printStackTrace();
+
+            fileOutputProducer.writeColumnNames();
+
+            int size = listToProcess.size();
+            double originalSize = size;
+            int counter = 1;
+
+            workInfo.setExportProgress("Exporting... " + size + " words.");
+
+            while (!listToProcess.isEmpty()) {
+                size = listToProcess.size();
+
+                if (counter % 51 == 0) {
+                    counter = 0;
+                    double currentSize = size;
+                    double percent = round(100 * (1 - (currentSize / originalSize)), 1);
+                    workInfo.setExportProgressValue(percent);
+                    workInfo.setExportProgress(percent + "% done.");
+                }
+
+                DocumentLocation docLocation = (DocumentLocation) listToProcess.get(size - 1);
+                listToProcess.remove(size - 1);
+
+                if (docLocation != null) {
+                    DocumentModel doc = session.getDocument(docLocation.getIdRef());
+
+                    if (doc != null) {
+                        List<FV_DataBinding> output = fileOutputProducer.readPropertiesWithReadersFrom(doc);
+
+                        assert (output != null) : "Null output from producer";
+
+                        if (output != null) {
+                            fileOutputProducer.writeRowData(output);
+                            counter++;
+                        }
+                    }
+                } else {
+                    log.warn("NUll docLocation in FVExportWorker.");
+                }
+            }
+
+            fileOutputProducer.close(session.getDocument(new IdRef(getDialectGUID())), getWorkInfo());
         }
+
     }
 }
